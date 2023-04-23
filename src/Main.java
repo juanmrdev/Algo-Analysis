481fa323505b40d6f1d3168c94b0e98767f97c7c
Juan Carlos Munoz Rojas <juan.mrdev@gmail.com>
2023-04-23 16:43:07 -0500

    First commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     static int count = 0;
 
     public static void main(String[] args) {
-
+//
         @ValName("Matriz 1")
         int[][] A = new int[n][n];
         @ValName("Matriz 2")
